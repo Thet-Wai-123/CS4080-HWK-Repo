24734e0c04d9e32a88c06ff7e8601c3950091527
Thet-Wai-123 <its.thetwai@gmail.com>
2026-04-15 20:01:42 -0700

    Completed Chapter 20 and 21

--- craftinginterpreters/c/compiler.c
+++ craftinginterpreters/c/compiler.c
@@ -412,6 +412,15 @@ static void parsePrecedence(Precedence precedence);
 //< Compiling Expressions forward-declarations
 //> Global Variables identifier-constant
 static uint8_t identifierConstant(Token* name) {
+    for (int i =0; i < currentChunk()->constants.count; i++) {
+    Value existing = currentChunk()->constants.values[i];
+    if (!IS_STRING(existing)) continue;
+
+    if (existingStr->length == name->length &&
+        memcmp(existingStr->chars, name->start, name->length) == 0) {
+      return (uint8_t)i;
+    }
+  }
   return makeConstant(OBJ_VAL(copyString(name->start,
                                          name->length)));
 }
